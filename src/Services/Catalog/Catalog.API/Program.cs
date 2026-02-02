50945d69b182da39abff4831aad47e0f3002e9b8
Ethelbert Marshall <remmarin@gmail.com>
2026-02-03 00:26:39 +0530

    Catalog API Boilerplate Code

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,12 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+WebApplication app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+//Configure the HTTP request pipeline.
+
+app.Run();
