f0178bf0d2c3547e6b03b7c544ce297991aa6741
Ethelbert Marshall <remmarin@gmail.com>
2026-02-04 00:58:44 +0530

    Create Abstraction on MediatR command and query handlers

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommand : ICommand<Unit> // Empty ICommand that doesnt return any response
+    {
+    }
+    public interface ICommand<out TResponse> : IRequest<TResponse> // ICommand that returns generic response
+    {
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    //Without Response
+    public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+         where TCommand : ICommand<Unit>
+    {
+    }
+
+    //With Response
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
