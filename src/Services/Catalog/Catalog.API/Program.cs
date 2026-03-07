2c79afd0b3e310cbbec187bde2a5b67647e012d2
Ethelbert Marshall <remmarin@gmail.com>
2026-03-07 22:13:15 +0530

    Implementing HealthChecks for Catalog API and Postgresql DB

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,8 @@
 
 
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the DI container.
@@ -26,13 +29,23 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
 
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
 
 app.MapCarter();
+
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+   
 
 //Global Exception Handling not required due to use of CustomExceptionHandler
 
