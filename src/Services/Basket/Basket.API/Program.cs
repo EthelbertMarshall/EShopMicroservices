c3a5e98dd034865446d16c5a59cfa854ca41e5ed
Ethelbert Marshall <remmarin@gmail.com>
2026-03-09 00:26:21 +0530

    Ad MediatR and Carter to DI

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,11 +1,18 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
 //Configure the HTTP request pipeline
-
+app.MapCarter();
 app.Run();
